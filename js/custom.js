705f13901a127f3903c37e44e940d51f321a786d
Aleksandar Timic <altim78@open.telekom.rs>
2017-05-28 14:02:42 +0200

    Cards section

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,13 @@
 $(document).ready(function(){
 
+    $("body").magnificPopup({
+        delegate: '.video',
+        type: 'iframe',
+        callbacks: {
+            beforeOpen: function () {
+                this.st.mainClass = this.st.el.attr('data-effect');
+            }
+        }
+    });
 
 });
