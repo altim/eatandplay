5ea0f82f1dff3912f6b3f010156642c86151078e
Aleksandar Timic <altim78@open.telekom.rs>
2017-05-28 20:36:10 +0200

    Logos

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
+    // Lightbox
     $("body").magnificPopup({
         delegate: '.video',
         type: 'iframe',
@@ -8,6 +9,23 @@ $(document).ready(function(){
                 this.st.mainClass = this.st.el.attr('data-effect');
             }
         }
+    });
+
+
+    //Logo slider
+    $('.logos-slider').bxSlider({
+        minSlider:1,
+        maxSlides:10,
+        sliderMargin: 75,
+        infiniteLoop: true,
+        touchEnabled : false,
+        pager : false,
+        controls: true,
+        auto : true,
+        pause: 5000,
+        nextSelector : '.logo-next',
+        prevSelector : '.logo-prev'
+
     });
 
 });
